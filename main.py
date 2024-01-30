e41f9f0ef43d20ec48f0fc1b3fa5ebe9f547a684
Joep van Seeventer <j.e.vanseeventer@student.utwente.nl>
2024-01-30 23:33:10 +0100

    do FSGM for several eps over imagenet and save to csv file, only one bug in the fsgm function

--- main.py
+++ main.py
@@ -1,26 +1,53 @@
-import tensorflow as tf
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.applications.inception_v3 import preprocess_input, InceptionV3
+from fsgm import ImageClassifier
+import torch
+import csv
 
-# Instantiate the inception-V3 pre-trained model
-model = InceptionV3()
-model.load_weights('inception_v3.ckpt')
+classifier = ImageClassifier()
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("Using device: {}".format(device))
 
-# print("Done")
-# # Assuming you have your NIPS dataset in a directory
-# test_datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
+image_paths, labels = classifier.get_image_paths(include_labels=True)
 
-# # Replace 'path_to_nips_dataset' with the path to your NIPS dataset
-# test_generator = test_datagen.flow_from_directory(
-#     'dataset/images',
-#     target_size=(299, 299),
-#     batch_size=32,
-#     class_mode='categorical')
+batch_size = 25
+epsilons = [0.1, 0.15, 0.2, 0.25, 0.3, 0.35]
+for eps in epsilons:
+    original_outputs_tot = []
+    original_confs_tot = []
+    perturbed_outputs_tot = []
+    perturbed_confs_tot = []
+    for i in range(0, len(image_paths), batch_size):
+        cur_image_paths = image_paths[i:i+batch_size]
+        cur_labels = labels[i:i+batch_size]
+        image_tensors = classifier.get_img_tensors(cur_image_paths)
+        outputs = classifier.classify_image(image_tensors)
+        cur_original_outputs, cur_original_confs = classifier.decode_predictions(outputs, include_conf=True)
+        original_outputs_tot[i:i+batch_size], original_confs_tot[i:i+batch_size] = cur_original_outputs.detach().numpy(), cur_original_confs.detach().numpy()
+        image_grads = classifier.get_grad(image_tensors, outputs, cur_labels)
+        perturbed_image_tensors = classifier.fgsm_attack(image_tensors, eps, image_grads)
+        perturbed_outputs = classifier.classify_perturbed_image(perturbed_image_tensors)
+        cur_perturbed_outputs, cur_perturbed_confs = classifier.decode_predictions(perturbed_outputs, include_conf=True)
+        perturbed_outputs_tot[i:i+batch_size], perturbed_confs_tot[i:i+batch_size] = cur_perturbed_outputs.detach().numpy(), cur_perturbed_confs.detach().numpy()
+        print(i)
 
-# # Evaluate the model
-# eval_result = model.evaluate(test_generator)
-# print(f'Test Loss: {eval_result[0]}, Test Accuracy: {eval_result[1]}')
+    output_file = "results_eps" + str(eps*10) + ".csv"
+    with open(output_file, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Original Output", "Original Confidence", "Perturbed Output", "Perturbed Confidence", "Label"])
+        for i in range(len(original_outputs_tot)):
+            writer.writerow([
+                original_outputs_tot[i],
+                original_confs_tot[i],
+                perturbed_outputs_tot[i],
+                perturbed_confs_tot[i],
+                labels[i]
+            ])
 
-# # Make predictions
-# #predictions = model.predict(test_generator)
-# # process predictions as needed
+### This is for plotting only one image
+# random_image, label = classifier.choose_random_image(include_label=True)
+# img_tensor = classifier.get_img_tensor(random_image)
+# original_outputs = classifier.classify_image(img_tensor)
+# img_grad = classifier.get_grad(img_tensor, original_outputs, label)
+# perturbed_img_tensor = classifier.fgsm_attack(img_tensor, 0.25, img_grad)
+# perturbed_outputs = classifier.classify_perturbed_image(perturbed_img_tensor)
+# # classifier.show_image(perturbed_img_tensor, outputs, label)
+# classifier.show_comparison(img_tensor, perturbed_img_tensor, original_outputs, perturbed_outputs, label)
